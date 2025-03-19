88732a64441fce31edc41b401a5ca088b2c60727
Malachi Alusa <malachialusa@gmail.com>
2025-03-19 14:24:06 -0600

    fixed typos

--- src/app/libs/data/SkillsSection.ts
+++ src/app/libs/data/SkillsSection.ts
@@ -12,12 +12,12 @@ export const playwirghtDescription =
 export const languagesTitle = "PROFICIENCIES";
 export const SkillsTitle = "SKILLS";
 export const cardData = [
-    {title: "TypeScript", description: "TypeScript is the launguage I am most familiar with. Its the main lanugage I use for Web Development. I have 4+ years of experience with it.", image: "/Icons/typeScriptIcon.png", subtitle: "Software Engineer"},
-    {title: "JavaScript", description: "I have a lot of expierence using JavaScript for both front and back end development. It was the original language I learnedd for development", image: "/Icons/javaScriptIcon.png", subtitle: "Software Engineer"},
-    {title: "C Sharp", description: "C# is a more recent language I have learned. I worked with it on  jr Developer Tickets while working at Tecuity.", image: "/Icons/cSharpIcon.png", subtitle: "Software Engineer"},
-    {title: "Microsoft SQL", description: "I learned SQL during my time as a QA associate. I queried tables, and modified databases daily to test software changes.", subtitle: "Software Engineer", image: '/Icons/sqlIcon-min.png'},
-    {title: "Google Pay", description: "I have expierence implementinig Google Pay. I've built integrations in professional sites using G pay as a means for checkout", image: "/Icons/gPayIcon.png", subtitle: "Software Engineer"},
-    {title: "Graph QL", description: "I've built multiple projects using node.js that utilize graph QL. Its a strong tool I'm happy to jump back into.", image: "/Icons/graphQLIcon.png", subtitle: "Software Engineer"},
-    {title: 'Git & CLI', description:'I am skilled with Git, and have worked with it for over 4 years. I also know command Line, and Power Shell', image: '/Icons/gitIcon.png', subtitle: 'Software Engineer'},
-    {title: 'CSS Frameworks', description: 'I know frameworks such as Tailwind, Bootstrap, and plain CSS. I have used these frameworks to build multiple applications.', image: '/Icons/cssIcon.png', subtitle: 'Software Engineer'},
+    {title: "TypeScript", description: "TypeScript is the language I am most familiar with. It's the main language I use for Web Development. I have 4+ years of experience with it.", image: "/Icons/typeScriptIcon.png", subtitle: "Software Engineer"},
+{title: "JavaScript", description: "I have a lot of experience using JavaScript for both front-end and back-end development. It was the original language I learned for development.", image: "/Icons/javaScriptIcon.png", subtitle: "Software Engineer"},
+{title: "C Sharp", description: "C# is a more recent language I have learned. I worked with it on junior Developer Tickets while working at Tecuity.", image: "/Icons/cSharpIcon.png", subtitle: "Software Engineer"},
+{title: "Microsoft SQL", description: "I learned SQL during my time as a QA Associate. I queried tables and modified databases daily to test software changes.", subtitle: "Software Engineer", image: '/Icons/sqlIcon-min.png'},
+{title: "Google Pay", description: "I have experience implementing Google Pay. I've built integrations in professional sites using G Pay as a means for checkout.", image: "/Icons/gPayIcon.png", subtitle: "Software Engineer"},
+{title: "GraphQL", description: "I've built multiple projects using Node.js that utilize GraphQL. It's a strong tool I'm happy to jump back into.", image: "/Icons/graphQLIcon.png", subtitle: "Software Engineer"},
+{title: "Git & CLI", description: "I am skilled with Git and have worked with it for over 4 years. I also know Command Line and PowerShell.", image: '/Icons/gitIcon.png', subtitle: 'Software Engineer'},
+{title: "CSS Frameworks", description: "I know frameworks such as Tailwind, Bootstrap, and plain CSS. I have used these frameworks to build multiple applications.", image: '/Icons/cssIcon.png', subtitle: 'Software Engineer'},
 ];

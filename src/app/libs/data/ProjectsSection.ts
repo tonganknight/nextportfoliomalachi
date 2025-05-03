cc9be00e5b286792b81e36538e67e477e06c7b3f
Malachi Alusa <malachialusa@gmail.com>
2025-05-03 08:54:32 -0600

    updated gif path

--- src/app/libs/data/ProjectsSection.ts
+++ src/app/libs/data/ProjectsSection.ts
@@ -1,5 +1,7 @@
-import { p } from "framer-motion/client"
-
+import portfolioGif from '../../../public/images/PortfolioGIF.gif';
+import peasAndThankYouGif from '../../../public/images/PeasAndThankYouGIF.gif';
+import TaskMasterProGif from '../../../public/images/TaskMasterProGIF.gif';
+import GitItDoneGif from '../../../public/images/GitItDoneGIF.gif';
 
 export const ProjectsSectionTitle = {
     title: "Projects",
@@ -10,26 +12,26 @@ export const ProjectsSectionCardData = [
     {projectTitle: 'Portfolio',
     projectDescription: 'This is my portfolio website, built with TypeScript, Next.js and Tailwind CSS. It my most recent work that showcases some of my talents and abilities. It is a responsive website that works on all devices.',
     projectLink: 'https://malachialusa-xi.vercel.app/',
-    projectImage: '/images/portfolio.png',
-    projectGIF: '/images/PortfolioGIF.gif',
+    projectImage: '/images/Portfolio.png',
+    projectGIF: portfolioGif,
     projectTags: ['TypeScript', 'Next.js', 'Tailwind CSS']},
     {projectTitle: 'Peas and Thank You', 
     projectDescription: 'This is a recipe website that I built with Next.js and Tailwind CSS utilizing Endamons API. It is a responsive website I built to get rid of left overs in the fridge',
     projectLink: 'https://peasandthankyou.vercel.app/',
     projectImage: '/images/PeasAndThankYou.png',
-    projectGIF: '/images/PeasAndThankYouGIF.gif',
+    projectGIF: peasAndThankYouGif,
     projectTags: ['TypeScript', 'Next.js', 'BootStrap', 'API']},
     {projectTitle:'TaskMaster Pro',
     projectDescription: 'This is a task management website that I built with React and Bootstrap CSS. It is a responsive website that works on all devices. It uses drag and drop functionality to move tasks around even on mobile.',
     projectLink: 'https://tonganknight.github.io/taskmaster-pro/',
     projectImage: '/images/TaskMasterPro.png',
-    projectGIF: '/images/TaskMasterProGIF.gif',
+    projectGIF: TaskMasterProGif,
     projectTags: ['JavaScript', 'React', 'Drag and Drop', ]},
     {projectTitle: 'Git It Done',
     projectDescription: 'This is an application that uses Git Hubs generous API to search for repositories and users, and filter issues by language.',
     projectLink: 'https://tonganknight.github.io/git-it-done/',
     projectImage: '/images/GitItDone.png',
-    projectGIF: '/images/GitItDoneGIF.gif',
+    projectGIF: GitItDoneGif,
     projectTags: ['Git', 'API', 'React', 'JavaScript', 'CSS' ]},
  {projectTitle: 'Run Buddy',
     projectDescription: 'This is a website that I built with HTML and CSS. It is a responsive website that works on all devices. It is a simple website that allows you to sign up for a running program.',

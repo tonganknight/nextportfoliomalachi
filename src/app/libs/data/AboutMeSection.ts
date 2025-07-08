308d260644bfe9b74ae98842b3b1b27902d4dd51
Malachi Alusa <malachialusa@gmail.com>
2025-07-08 17:31:34 -0600

    removed unused code

--- src/app/libs/data/AboutMeSection.ts
+++
@@ -1,7 +0,0 @@
-
-
-export const AboutMeData = {
-    photoSRC: "https://cdn.iconfinder.com/stored_data/2033254/128/png?token=1734196675-hE6eiMJenAbV15VW4FgFA3kUjDXRyl6OyEPVYlm%2BWKg%3D",
-    P1: "Hi, I’m Malachi — a driven and adaptable full stack developer with a strong foundation in modern JavaScript frameworks like React, Angular, and Next.js, and real-world experience in building scalable, user-focused web applications.At Nature’s Sunshine, I help bring digital projects to life, collaborating across teams to create seamless UI experiences. My prior work at WETx and Tecuity deepened my testing and QA expertise, where I wrote automated regression tests using Playwright and TypeScript, ensuring high product reliability for state-level software systems.",
-    P2: "What sets me apart is my unique blend of software development and testing experience. I understand what makes applications not only functional, but also stable, performant, and user-friendly. Whether it’s squashing bugs, shipping features, or optimizing the dev workflow, I bring a team-first mentality and a hunger to keep learning. If you're hiring a junior-to-mid-level dev with a strong work ethic and a solid foundation — let’s connect!"
-};
